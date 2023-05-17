7daabf071d2ba09931c23ff7be8605cf5ca5be36
William <eh.canoidian@gmail.com>
2023-05-16 20:21:45 -0400

    36:32

---
+++ Character/LandingState.gd
@@ -0,0 +1,7 @@
+extends State
+
+class_name LandingState
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
--- Character/player.gd
+++ Character/player.gd
@@ -5,46 +5,81 @@ extends CharacterBody2D
 @export var jump_velocity : float = -150.0
 @export var double_jump_velocity : float = -100
 
-@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var sprite : Sprite2D = $Sprite2D
+@onready var animation_tree : AnimationTree = $AnimationTree
+@onready var state_machine : CharacterStateMachine = $CharacterStateMachine
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var has_double_jumped : bool = false
 var animation_locked : bool = false
-var an_direction : Vector2 = Vector2.ZERO
+var direction : Vector2 = Vector2.ZERO
+var was_in_air : bool = false
+
+func _ready():
+	animation_tree.active = true
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		was_in_air = true
 	else:
 		has_double_jumped = false
+		
+		if was_in_air == true:
+			land()
+		was_in_air = false
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			# normal jump
-			velocity.y = jump_velocity
+			jump()
 		elif not has_double_jumped:
 			# doulbe jump in air
-			velocity.y += double_jump_velocity
-			has_double_jumped = true
+			double_jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var	direction = Input.get_axis("left", "right")
+	direction = Input.get_vector("left", "right", "up", "down")
 	
-	if direction:
-		velocity.x = direction * speed
+	if direction.x != 0 and state_machine.check_if_can_move():
+		velocity.x = direction.x * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+	update_animation()
+	update_facing_direction()
 
 
 func update_animation():
-	if not animation_locked:
-		if an_direction != Vector2.ZERO:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("idle")
+	animation_tree.set("parameters/Move/blend_position", direction.x)
+	
+				
+func update_facing_direction():
+	if direction.x > 0:
+		sprite.flip_h = false
+	elif direction.x < 0:
+		sprite.flip_h = true
+
+func jump():
+	velocity.y = jump_velocity
+	# animated_sprite.play("jump_start")
+	animation_locked = true
+	
+func double_jump():
+	velocity.y = double_jump_velocity
+	# animated_sprite.play("jump_double")
+	animation_locked = true
+	has_double_jumped = true
+
+func land():
+	# animated_sprite.play("jump_end")
+	animation_locked = true
+
+#func _on_animated_sprite_2d_animation_finished():
+#	if (["jump_end", "jump_start", "jump_double"].has(animated_sprite.animation)):
+#		animation_locked = false
+	
